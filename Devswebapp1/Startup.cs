081a2a45eae7cac69d7c629a69c90fe120a4e5ca
Devavrathan Nair <devavrathan@outlook.com>
2018-11-03 07:47:59 +0000

    Devs

---
+++ Devswebapp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Devswebapp1.Startup))]
+namespace Devswebapp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
